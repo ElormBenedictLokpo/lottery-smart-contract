640625e424cc314b4bf42d92cb0d730373595942
ElormBenedictLokpo <officiallokpoelorm@gmail>
2023-10-06 10:40:46 +0000

    :construction: winner declaration

--- lottery.sol
+++ lottery.sol
@@ -5,25 +5,47 @@ pragma solidity ^0.8.0;
 contract Lottery{
     address[] public participants;
     address public lottery_manager;
+    address payable public winner_address;
+
 
     constructor(){
         lottery_manager = msg.sender;
     }
+
+    event ParticipantJioned(address _participant);
 
     modifier minimumValueRequirement(){
         require(msg.value >= 0.000000000000000001 ether, "Minimum is 0.01 ether to enter lottery");
         _;
     }
 
-    function viewParticipants() public view returns(address[] memory){
+    modifier isManager(){
+        require(msg.sender == lottery_manager);
+        _;
+    }
+
+    function viewParticipants() public view isManager returns(address[] memory){
         return participants;
     }
 
-    function addParticipant() public payable minimumValueRequirement{
+    function payToJoin() public payable minimumValueRequirement{
         participants.push(msg.sender);
+
+        emit ParticipantJioned(msg.sender);
     }
 
-    function getLotteryBalance() public view returns (uint256){
+    function getLotteryBalance() public view isManager returns (uint256){
         return address(this).balance;
+    }
+
+    function declareWinner() public isManager{
+        winner_address = payable(participants[1]);
+        winner_address.transfer(address(this).balance);
+
+        participants = new address[](0);
+    }
+
+    function getBalanceOfParticipant(address _address) public view isManager returns(uint256){
+        return _address.balance;
     }
 }
